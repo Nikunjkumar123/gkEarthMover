04241828293be251308e96e371a90f9cbce3507b
nikunj kumar <nikunjkumar2911@gmail.com>
2025-02-03 12:42:56 +0530

    asd

--- Earth-Mover-Backend/index.js
+++ Earth-Mover-Backend/index.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(fileUpload({useTempFiles:true}));
 app.use(cors({
-    origin:['www.dushadinfra.com','wwww.admin.dushadinfra.com'],
+    origin:['https://www.dushadinfra.com','https://wwww.admin.dushadinfra.com'],
     methods: ["GET", "POST", "PATCH", "DELETE"],
     credentials: true,
 }))
